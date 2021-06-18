4df934a5ba6049c8e67627e2c1f3394787768e78
MakiseKurisu-004 <83620606+MakiseKurisu-004@users.noreply.github.com>
2021-06-18 12:55:16 -0400

    Add files via upload

---
+++ komachichan.py
@@ -0,0 +1,45 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("/Users/alfredleong/Downloads/data.json"))
+data =  {k.lower(): v for k, v in data.items()}
+
+def di(dicti):
+    x = data[dicti]
+    c = ' '.join(x)
+    return ("{}, Onii-chan~.").format(c)
+
+def nt(user_input):
+    if user_input in data:
+        b = di(user_input)
+        return b
+    
+    elif len(get_close_matches(user_input, data.keys())) > 0:
+        j = get_close_matches(user_input, data.keys())[0]
+        yn = input(f"Did you mean {j}, Onii-chan~? Y for yes and N for no: ")
+        yn = yn.upper()
+        if yn == 'Y':
+            v = get_close_matches(user_input, data.keys())[0]
+            r = di(v)
+            return r
+        elif yn == 'N':
+            return 'What word do want to define, Onii-chan~: '
+        else:
+            return 'Komachi-chan doens\'t understand'
+        
+    else:    
+        return 'Onii-chan, are you stupid? That word doesn\'t exist.'
+
+print('Welcome Back Onii-chan~!')
+while True:
+    user_input = input('What word do want to define, Onii-chan~: ')
+    user_input = user_input.lower()   
+    
+    if user_input == 'byebye':
+        h = 'Bye Onii-chan~'
+        break
+    else:
+        uu = nt(user_input)
+        if uu != 'What word do want to define, Onii-chan~: ':
+            print(uu)
+        continue
+print(h)
